1a993f09efe57be472dabe7d6d501cab679ac83b
ice-rahul <meet4g@gmail.com>
2021-03-18 02:26:14 +0530

    First story book commit with sample stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,25 +1,10 @@
 module.exports = {
-  stories: ['../src/**/*.stories.tsx'],
-  webpackFinal: async config => {
-    const assetRule = config.module.rules.find(({ test }) => test.test('.svg'))
-
-    const assetLoader = {
-      loader: assetRule.loader,
-      options: assetRule.options || assetRule.query,
-    }
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve('ts-loader'),
-        },
-      ],
-    })
-    config.module.rules.unshift({
-      test: /\.svg$/,
-      use: ['@svgr/webpack', assetLoader],
-    })
-    config.resolve.extensions.push('.ts', '.tsx')
-    return config
-  },
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,4 @@
-import React from 'react'
-import { addDecorator } from '@storybook/react'
-import Layout from './Layout'
 
-addDecorator(storyFn => <Layout>{storyFn()}</Layout>)
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+}
